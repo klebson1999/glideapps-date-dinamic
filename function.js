2ac49ef766d2f2e774e8ca34d64c4853a0e5a826
Clebson <klebson1999@gmail.com>
2022-05-16 17:14:53 -0300

    Resolvendo data do mês de janeiro e removendo referências a lib Moments.

--- function.js
+++ function.js
@@ -1,56 +1,60 @@
-window.function = function (dia) {
-    const dateToday = moment(Date.now())
+
+window.function = function dateSett() {
+    const dateToday = new Date()
     // Subtraindo e retirando o 'ano' e o 'mês' do resultado
-    let dateBack = moment(dateToday).subtract(20, 'days')
-    let dateYear = dateBack.format('YYYY')
-    let numberMounth = dateBack.format('MM')
+    let todayMounthNumber = dateToday.getMonth()
+    //let dateYear = dateBack.format('YYYY')
+    //let numberMounth = dateBack.format('MM')
     // Nome do mês
-    switch (numberMounth) {
-        case '01':
-            nameMounth = "Janeiro";
+    switch (todayMounthNumber) {
+        case 0:
+            todayMounthNumber = "Janeiro";
             break;
-        case '02':
-            nameMounth = "Fevereiro";
+        case 1:
+            todayMounthNumber = "Fevereiro";
             break;
-        case '03':
-            nameMounth = "Março";
+        case 2:
+            todayMounthNumber = "Março";
             break;
-        case '04':
-            nameMounth = "Abril";
+        case 3:
+            todayMounthNumber = "Abril";
             break;
-        case '5':
-            nameMounth = "Maio";
+        case 4:
+            todayMounthNumber = "Maio";
             break;
-        case '6':
-            nameMounth = "Junho";
+        case 5:
+            todayMounthNumber = "Junho";
             break;
-        case '7':
-            nameMounth = "Julho";
+        case 6:
+            todayMounthNumber = "Julho";
             break;
-        case '8':
-            nameMounth = "Agosto";
+        case 7:
+            todayMounthNumber = "Agosto";
             break;
-        case '9':
-            nameMounth = "Setembro";
+        case 8:
+            todayMounthNumber = "Setembro";
             break;
-        case '10':
-            nameMounth = "Outubro";
+        case 9:
+            todayMounthNumber = "Outubro";
             break;
-        case '11':
-            nameMounth = "Novembro";
+        case 10:
+            todayMounthNumber = "Novembro";
             break;
-        case '12':
-            nameMounth = "Dezembro";
+        case 11:
+            todayMounthNumber = "Dezembro";
             break;
         default:
-            nameMounth = "Mês inexistente";
+            todayMounthNumber = "Mês inexistente";
     }
     // Data inicial e final de envio 
     let dateDayInit = 1
     let dateDayEnd = 15
-    let dateYearToday = dateToday.format('YYYY')
-    let numberMounthToday = dateToday.format('MM')
-
+    let dateYearToday = dateToday.getFullYear()
+    let numberMounthToday = dateToday.getMonth() +2
+    if (numberMounthToday === 13) {
+        numberMounthToday = 01
+        dateYearToday = dateToday.getFullYear() +1
+    } 
     let concatDateInit = dateYearToday + '-' + numberMounthToday + '-' + dateDayInit
     let concatDateEnd = dateYearToday + '-' + numberMounthToday + '-' + dateDayEnd
     let parseDateInit = new Date(concatDateInit)
@@ -58,8 +62,7 @@ window.function = function (dia) {
     // Verificando se a data atual é válida
     let resultIfDate = dateToday >= parseDateInit && dateToday <= parseDateEnd ? 'FALSE' : 'TRUE'
 
-    let dateReturn = [numberMounth, nameMounth, dateYear, parseDateEnd, resultIfDate]
+    let dateReturn = [todayMounthNumber, dateYearToday, parseDateEnd, resultIfDate]
 
     return dateReturn
 }
-
